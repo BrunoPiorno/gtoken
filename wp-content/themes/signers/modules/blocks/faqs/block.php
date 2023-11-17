40c460725be92cbffde41ef76d990ffeda52ab93
brunopiorno <brunopiornop@gmail.com>
2023-11-17 14:38:33 -0300

    faqs

---
+++ wp-content/themes/signers/modules/blocks/faqs/block.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Text/image
+ *
+ * Title:       Faqs
+ * Description: Faqs
+ * Category:    palermo
+ * Icon:        columns
+ * Keywords:    facts
+ * Post Types:  all
+ * Multiple:    true
+ * Active:      true
+ * Styles:      
+ * CSS Deps:
+ * JS Deps:
+ *
+ * @package Palermo
+ * @subpackage defaultTheme
+ * @since defaultTheme  1.0
+ */
+
+ do_action( 'palermo_pre_render_block', $block );
+?>
