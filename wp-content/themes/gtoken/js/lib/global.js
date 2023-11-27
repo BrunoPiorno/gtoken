84e1f098520c147306216bc8d32752fea684526e
brunopiorno <brunopiornop@gmail.com>
2023-11-27 12:31:22 -0300

    slider links

--- wp-content/themes/gtoken/js/lib/global.js
+++ wp-content/themes/gtoken/js/lib/global.js
@@ -115,5 +115,16 @@ jQuery(document).ready(function ($) {
 			jQuery('.site-menu__item.hover').removeClass('hover');
 		}
 	})
-
+	var swiper = new Swiper('.swiper-container', {
+		slidesPerView: 3,
+		spaceBetween: 30,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
 });
---
+++ wp-content/themes/gtoken/modules/blocks/slider-links/block.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Text/image
+ *
+ * Title:       Slider Links
+ * Description: Slider
+ * Category:    palermo
+ * Icon:        columns
+ * Keywords:    facts
+ * Post Types:  all
+ * Multiple:    true
+ * Active:      true
+ * Styles:      
+ * CSS Deps:
+ * JS Deps:
+ *
+ * @package Palermo
+ * @subpackage defaultTheme
+ * @since defaultTheme  1.0
+ */
+
+ do_action( 'palermo_pre_render_block', $block );
+?>
+<?php if( have_rows('repeater_field') ): ?>
+    <section class="slider-links">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <?php while( have_rows('repeater_field') ): the_row(); 
+                    $logo = get_sub_field('logo');
+                    $link = get_sub_field('link');
+                    $texto = get_sub_field('texto'); ?>
+                    
+                    <div class="swiper-slide">
+                        <?php echo '<a href="' . esc_url($link['url']) . '" target="' . esc_attr($link['target']) . '">';
+                            if($logo) { ?>
+                                <?php get_template_part('modules/components/image', null, ['image' => $logo]); ?>
+                            <?php } 
+
+                            echo '<p>' . esc_html($texto) . '</p>';
+                        echo '</a>';?>
+                    </div>
+                <?php endwhile; ?>
+            </div>
+        </div>
+    </section>
+<?php endif; ?>
+
