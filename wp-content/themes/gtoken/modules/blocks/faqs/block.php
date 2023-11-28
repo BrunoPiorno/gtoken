7f1aa9efe38bde6acff57dc5301b9c38ed5775a5
Enrique Scapoli <88351578+EnriqueHS@users.noreply.github.com>
2023-11-28 17:20:14 -0300

    scss

--- wp-content/themes/gtoken/modules/blocks/faqs/block.php
+++ wp-content/themes/gtoken/modules/blocks/faqs/block.php
@@ -21,18 +21,19 @@
 
  do_action( 'palermo_pre_render_block', $block );
 ?>
-
-<section class="accordion accordion--faqs" id="faqs">
-    <div class="container">
-        <div class="accordion__cont">
-            <?php while (have_rows('faqs')): the_row();
-                if ($question = get_sub_field('question')): ?>
-                    <div class="accordion__item">
-                        <h3 class="accordion__question h3 js-accordion-open" tabindex="0"><?php echo $question; ?></h3>
-                        <?php echo ($answer = get_sub_field('answer')) ? '<div class="accordion__answer">' . $answer . '</div>' : ''; ?>
-                    </div>
-                <?php endif;
-            endwhile; ?>
+<section class="wrapper wrapper--light-black">
+    <section class="accordion accordion--faqs" id="faqs">
+        <div class="container">
+            <div class="accordion__cont">
+                <?php while (have_rows('faqs')): the_row();
+                    if ($question = get_sub_field('question')): ?>
+                        <div class="accordion__item">
+                            <h3 class="accordion__question h3 js-accordion-open" tabindex="0"><?php echo $question; ?></h3>
+                            <?php echo ($answer = get_sub_field('answer')) ? '<div class="accordion__answer">' . $answer . '</div>' : ''; ?>
+                        </div>
+                    <?php endif;
+                endwhile; ?>
+            </div>
         </div>
-    </div>
+    </section>
 </section>
