832b7ab4ab168bf347aa3ebfb61b18cd9a1ea447
brunopiorno <brunopiornop@gmail.com>
2023-12-01 10:49:45 -0300

    js

--- wp-content/themes/gtoken/js/lib/global.js
+++ wp-content/themes/gtoken/js/lib/global.js
@@ -18,6 +18,7 @@ jQuery(document).ready(function ($) {
 		$(e.target).removeClass('active');
 	});
 
+
 	$(window).scroll(function () {
 		if ($(window).scrollTop() > 50) {
 			$('body').addClass('scrolled');
@@ -30,40 +31,6 @@ jQuery(document).ready(function ($) {
 		$('body').toggleClass('menuopen');
 	});
 
-	$('.with-submenu .site-menu__first-level').click(function (e) {
-		if ($(window).width() < 1051) {
-			e.preventDefault();
-
-			p = $(this).closest('.with-submenu');
-
-			if ($('.submenu', p).attr('aria-hidden') == 'false') {
-
-				$('.submenu', p).attr('aria-hidden', 'true');
-				$(this).attr('aria-selected', 'true');
-			} else {
-				$('.submenu', p).attr('aria-hidden', 'false');
-				$(this).attr('aria-selected', 'false');
-
-			}
-
-		}
-	});
-
-	$(window).scroll(() => {
-
-		if ($('.block-tabs .tabs-content-in').css('position') == 'sticky') {
-			currentScroll = $('.tabs-content-out').position().top - $(window).scrollTop();
-			if (currentScroll < 0) {
-
-				currentitem = Math.floor(currentScroll / 1000) * -1 - 1;
-
-				//only activate de button if is not selected
-				if ($('.tabs li:eq(' + currentitem + ') button').attr('aria-selected') !== 'true')
-					$('.tabs li:eq(' + currentitem + ') button').click();
-			}
-		}
-
-	});
 
 	jQuery('body').on('click', function (e) {
 		if (jQuery(e.target).closest('.site-menu').length == 0) {
@@ -102,5 +69,8 @@ jQuery(document).ready(function ($) {
 		new Swiper('.slider-partners', opciones);
 	}
 
+	jQuery('.site-menu__first-level').on('click', () => {
+		jQuery('body').removeClass('menuopen');
+	});
 
 });
